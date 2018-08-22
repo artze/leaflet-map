180999e5b5ea26046d9bc74e2121e7c7ea72aa46
artze <arthurwtm@gmail.com>
2018-08-22 20:50:19 +0200

    Initialize map on page

---
+++ src/components/LeafletMap.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="map"></div>
+</template>
+
+<script>
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+
+export default {
+    methods: {
+        addMapToView() {
+            let map = L.map('map').setView([51.505, -0.09], 13)
+            L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                maxZoom: 18,
+                id: 'mapbox.streets',
+                accessToken: 'pk.eyJ1IjoiYXJ0emUiLCJhIjoiY2psNWdxZ3g1MHA5dDN4cGJ4Ym93Mnl6YyJ9.OJ2PstZSXAnFJa5UMqwS5w'
+            }).addTo(map)
+        }
+    },
+    mounted() {
+        this.addMapToView()
+    }
+}
+</script>
+
+<style scoped>
+    #map{
+        height: 500px;
+        width: auto;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import LeafletMap from '@/components/LeafletMap'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            component: LeafletMap
+        }
+    ]
 })
