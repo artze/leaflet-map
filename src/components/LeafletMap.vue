4e26e88bea1eef9db005437cb3996cfd0360c3ae
artze <arthurwtm@gmail.com>
2018-08-22 22:26:06 +0200

    Add 100 markers at each location cycle

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -1,25 +1,58 @@
 <template>
-    <div id="map"></div>
+    <div>
+        <div id="map"></div>
+        <button @click="changeLocation">Change Location</button>
+    </div>
 </template>
 
 <script>
-import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
 
 export default {
+    data() {
+        return {
+            map: '',
+            viewCoordArr: [[51.505, -0.09], [22.286, 114.150], [52.520, 13.405], [13.737, 100.523]],
+            cycleCounter: 0
+        }
+    },
     methods: {
-        addMapToView() {
-            let map = L.map('map').setView([51.505, -0.09], 13)
+        addMapTiles() {
+            this.map.setView([51.505, -0.09], 15)
             L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
                 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
                 maxZoom: 18,
                 id: 'mapbox.streets',
                 accessToken: 'pk.eyJ1IjoiYXJ0emUiLCJhIjoiY2psNWdxZ3g1MHA5dDN4cGJ4Ym93Mnl6YyJ9.OJ2PstZSXAnFJa5UMqwS5w'
-            }).addTo(map)
+            }).addTo(this.map)
+        },
+        changeLocation() {
+            if(this.cycleCounter === this.viewCoordArr.length - 1) {
+                this.cycleCounter = 0
+            } else {
+                this.cycleCounter++
+            }
+            this.map.setView(this.viewCoordArr[this.cycleCounter], 15)
+            this.addMarkers()
+        },
+        addMarkers() {
+            let markerCoordArr = []
+            let lat = this.viewCoordArr[this.cycleCounter][0]
+            let lon = this.viewCoordArr[this.cycleCounter][1]
+            for(let i = 0; i < 100; i++) {
+                lat += 0.0001
+                lon += 0.0001
+                markerCoordArr.push([lat, lon])
+            }
+            console.log(markerCoordArr)
+            markerCoordArr.forEach((coord) => {
+                L.marker(coord).addTo(this.map)
+            })
         }
     },
     mounted() {
-        this.addMapToView()
+        this.map = L.map('map')
+        this.addMapTiles()
     }
 }
 </script>
